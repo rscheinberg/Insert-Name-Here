66f41485bcd66dc208de9e2d7fa6adb41d4f702e
rscheinberg <rscheinberg@stuy.edu>
2018-04-11 23:20:33 -0400

    updated MazeSolver classes, with basic heading, fields, and constructor, as needed as of this push

---
+++ Java code/MazeSolver.java
@@ -0,0 +1,17 @@
+/*
+* Class whose purpose is to encapsulate a Maze that can be solved by the method solveMaze()
+*/
+
+public class MazeSolver {
+	
+	private Maze current;
+	
+	public MazeSolver (Maze encapsulate) {
+		current = encapsulate;
+	}
+	
+	public boolean solveMaze () {
+		return solveMaze();
+	}
+	
+}
